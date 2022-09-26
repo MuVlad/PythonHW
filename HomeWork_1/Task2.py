471029ce618fd032f6921ba42e80d9e9454d3918
Vlad Muslimov <muslimov.vlad@mail.ru>
2022-09-26 20:35:47 +0500

    Done task2

--- HomeWork_1/Task2.py
+++ HomeWork_1/Task2.py
@@ -1,2 +1,12 @@
 # задача 2. Напишите программу для. проверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+x = int(input("Введите значение переменой X "))
+y = int(input("Введите значение переменой Y "))
+z = int(input("Введите значение переменой Z "))
+
+first_predicate = not(x or y or z)
+second_predicate = not x and (not y) and (not z)
+result = first_predicate == second_predicate
+
+print(result)
