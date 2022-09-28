5694c5ce37c25c97dfbd1d193f9cb065734d68ce
Vlad Muslimov <muslimov.vlad@mail.ru>
2022-09-29 00:15:24 +0500

    Done task3

--- HomeWork_2/Task3.py
+++ HomeWork_2/Task3.py
@@ -2,7 +2,19 @@
 # Список размерностью 10 задается случайными целыми числами, выводится на экран, 
 # затем перемешивается, опять выводится на экран. SHUFFLE нельзя юзать!
 
-from random import Random
+from random import Random, randint
 
 
-random_list = Random()
+random_list = []
+for i in range(10):
+    random_list.append(randint(-10,10))
+print(random_list)
+
+
+for i in range(len(random_list) -1):
+   swap = randint(0,len(random_list) -1)
+   temp =random_list[swap]
+   random_list[swap] = random_list[i]
+   random_list[i] = temp
+
+print(random_list)
