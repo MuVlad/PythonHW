f202564da963988b843850d9b9bb5f0f5215e366
Vlad Muslimov <muslimov.vlad@mail.ru>
2022-09-29 19:46:09 +0500

    "Done task1"

--- HomeWork_2/Task1.py
+++ HomeWork_2/Task1.py
@@ -4,11 +4,16 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-number = int(float(input('Введите число: ')))
-sum = 0
-while number > 0:
-    sum += number % 10
-    number = number//10
+number = float(input('Введите число: '))
+whole_part = int(number)
+fractional_part = number - int(number)
 
+sum = 0
+while whole_part > 0:
+    sum += whole_part % 10
+    whole_part = whole_part // 10
+while fractional_part > 0:
+    sum += int(fractional_part * 10)
+    fractional_part = round(fractional_part * 10 - int(fractional_part * 10),5)
 
 print(sum)
