0d45754e765da5f54fcf7e8e8322e3eea64b8ebd
Vlad Muslimov <muslimov.vlad@mail.ru>
2022-10-10 21:26:00 +0500

    Update task1

--- HomeWork_5/Task_1_Bot.py
+++ HomeWork_5/Task_1_Bot.py
@@ -7,18 +7,17 @@
 # b) Подумайте как наделить бота ""интеллектом""
 from random import randint
 
-
 def greetings():
     print("Добро пожаловать в игру конфеты!")
     print(" ")
-    print("Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга.\n"
+    print("Условие задачи: На столе лежит 2021 конфета. Вы играете против Бота, делая ход друг после друга.\n"
           "Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.\n"
           "Все конфеты оппонента достаются сделавшему последний ход. Удачи!")
     print(" ")
 
 
 def processing_user_input(name):
-    counter = int(input(f"Ходит игрок {name}, введите колличество конфет, которое хотите взять (от 1 до 28 штук): "))
+    counter = int(input(f"Ходит {name}, введите колличество конфет, которое хотите взять (от 1 до 28 штук): "))
     while counter < 1 or counter > 28:
         counter = int(input("Количество конфет должно быть в диапазоне (от 1 до 28), попробуйте еще раз: "))
     return counter
@@ -26,38 +25,38 @@ def processing_user_input(name):
 
 def result_game(winner):
     if winner:
-        return player1
+        return player
     else:
-        return player2
+        return Bot_Vasya
 
 
 number_candies = 2021
 greetings()
 
-player1 = input('Введите имя первого игрока: ')
-player2 = input('Введите имя втрого игрока: ')
+player = input('Введите имя игрока: ')
+Bot_Vasya = "Бот Вася"
 walker = randint(0, 2)
-player_counter1 = 0
-player_counter2 = 0
+player_counter = 0
+Bot_Vasya_counter = 0
 print(" ")
 print(f"На столе {number_candies} конфет(а)")
 
 while number_candies > 28:
     if walker:
-        temp = processing_user_input(player1)
-        player_counter1 += temp
+        temp = processing_user_input(player)
+        player_counter += temp
         number_candies -= temp
-        print(f"Игрок {player1} взял {temp} конфет(ы), теперь у него их {player_counter1}")
+        print(f"{player} взял {temp} конфет(ы), теперь у него их {player_counter}")
         print(f"На столе {number_candies} конфет(а)")
         print(" ")
         walker = False
     else:
-        temp = processing_user_input(player2)
-        player_counter2 += temp
+        temp = randint(1,29)
+        Bot_Vasya_counter += temp
         number_candies -= temp
-        print(f"Игрок {player2} взял {temp} конфет(ы), теперь у него их {player_counter2}")
+        print(f"{Bot_Vasya} взял {temp} конфет(ы), теперь у него их {Bot_Vasya_counter}")
         print(f"На столе {number_candies} конфет(а)")
         print(" ")
         walker = True
 
-print(f"Игрок {result_game(walker)}, поздравляем вас, последний ход за вами, вы выиграли!")
+print(f"{result_game(walker)}, поздравляем вас, последний ход за вами, вы выиграли!")
